9d196829c131decc14732d70fcd5d983f9818597
Lynn "Limonade <129105107+Lynn26621@users.noreply.github.com>
2025-09-22 11:18:45 -0400

    9/22/25
    added instructions and todo

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+/*
+HOW TO SET UP
+
+run {
+    npm i
+    node app
+    }
+go to localhost:3000
+
+download formbar dev branch from https://github.com/csmith1188/Formbar.js/tree/DEV
+in formbar window, run {
+    npm i;
+    cp .env-template .env;
+    npm run init-db;
+    node app;
+    }
+go to localhost:420
+log in (or register)
+
+*/
+
+/* TODO:
+[] Send user permissions, formbar_id, and username from classroomData to Polls.ejs (so it can be used for the db)
+[] Make a page that displays the relevant data for the current class and polls via websockets
+[] Only users with the correct permissions can create polls
+*/
+
 let express = require('express');
 let app = express();
 const { io } = require('socket.io-client');
-const FORMBAR_URL = 'http://localhost:420'  //'http://172.16.3.159:420/';
-const API_KEY = 'aa3663be018501ad55c4c1c6ef1ca0073704586be7f11c74849daf3fed035f6d';
+const FORMBAR_URL = 'http://localhost:420'  //'http://formbeta.yorktechapps.com';
+const API_KEY = 'aa3663be018501ad55c4c1c6ef1ca0073704586be7f11c74849daf3fed035f6d'; // Your API key here
 
 const socket = io(FORMBAR_URL, {
     extraHeaders: {
@@ -45,3 +72,4 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000);
+
